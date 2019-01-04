a29e0dcaf67ce40c30544c17e82bf09e7a0b893a
hutwagner tate <hutwagnert@gmail.com>
2019-01-04 18:42:54 -0500

    working need to clean up html and old data and heroku

--- express/frieder/routes/htmlRoutes.js
+++ express/frieder/routes/htmlRoutes.js
@@ -1,7 +1,4 @@
-// ===============================================================================
-// DEPENDENCIES
-// We need to include the path package to get the correct file path for our html
-// ===============================================================================
+
 var path = require("path");
 
 
@@ -10,10 +7,6 @@ var path = require("path");
 // ===============================================================================
 
 module.exports = function(app) {
-  // HTML GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases the user is shown an HTML page of content
-  // ---------------------------------------------------------------------------
 
   app.get("/survey", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/survey.html"));
@@ -21,8 +14,6 @@ module.exports = function(app) {
   });
 
 
-
-  // If no matching route is found default to home
   app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/home.html"));
     console.log("home");
