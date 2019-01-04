a39dcbbe5363fe436ad1390b5d51bcb151291644
hutwagner tate <hutwagnert@gmail.com>
2019-01-04 09:32:53 -0500

    i can get the object

--- express/frieder/data/tableData.js
+++ express/frieder/data/tableData.js
@@ -7,8 +7,8 @@
 
 var tableArray = [
   {
-    customerName: "Ahmed",
-    customerEmail: "ahmed@example.com",
+    name: "Ahmed",
+    photo: "https://images.mentalfloss.com/sites/default/files/styles/mf_image_16x9/public/62012-istock-833768276.jpg?itok=AvAKdWF_&resize=1100x1100",
     customerID: "afhaque89",
     phoneNumber: "000-000-0000"
   }
