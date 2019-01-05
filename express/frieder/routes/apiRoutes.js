a4500874c775e5bb46414e81980ad2084bdd8569
hutwagner tate <hutwagnert@gmail.com>
2019-01-05 12:02:36 -0500

    html looking better heroku left

--- express/frieder/routes/apiRoutes.js
+++ express/frieder/routes/apiRoutes.js
@@ -9,15 +9,11 @@ var tableData = require("../data/tableData");
 
 module.exports = function(app) {
   // API GET Requests
-
-
   app.get("/api/tables", function(req, res) {
     res.json(tableData);
   });
-
   // API POST Requests
   app.post("/api/tables", function(req, res) {
-
       tableData.push(req.body);
       res.json(true);   
   });
